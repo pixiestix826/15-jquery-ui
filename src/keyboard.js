2f1cee87da6e136f039c9e8a6c80632221f0c1e8
Brandi Soucy <bbsoucy@gmail.com>
2015-11-13 15:26:31 -0600

    reworking html markup for better control over functionality

---
+++ src/keyboard.js
@@ -0,0 +1,11 @@
+$(function() {
+  var $write = $(`.text-box`),
+  var shift = false,
+  var capslock = false,
+
+$(`.keys`).click(function() {
+  var $this = $(this),
+      character = $this.html();
+
+});
+});
