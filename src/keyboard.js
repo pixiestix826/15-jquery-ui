a0eb685d35dad24b7febf29f23a2efa1fe3bd1e9
Brandi Soucy <bbsoucy@gmail.com>
2015-11-13 16:41:49 -0600

    Styling changes on accordion

--- src/keyboard.js
+++ src/keyboard.js
@@ -1,11 +1,11 @@
-$(function() {
-  var $write = $(`.text-box`),
-  var shift = false,
-  var capslock = false,
-
-$(`.keys`).click(function() {
-  var $this = $(this),
-      character = $this.html();
-
-});
-});
+// $(function() {
+//   var $write = $(`.text-box`),
+//   var shift = false,
+//   var capslock = false,
+//
+// $(`.keys`).click(function() {
+//   var $this = $(this),
+//       character = $this.html();
+//
+// });
+// });
