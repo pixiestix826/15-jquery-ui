95fcf029092a91281d4a8908abfbfd40ee65f8fd
Brandi Soucy <bbsoucy@gmail.com>
2015-11-14 15:59:50 -0600

    keyboard js

--- src/keyboard.js
+++ src/keyboard.js
@@ -1,49 +1,55 @@
 $(function() {
   var $write = $(`.write`),
-      shift = false,
-      capslock = false,
+      // shift = false
+      // capslock = false
 
- $ (`.keyboard-keys li`).click(function() {
+  $(`.keyboard-keys li`).click(function() {
     var $this = $(this),
         character = $this.html(); // If it's a lowercase letter, nothing happens to this variable
-        //Shift keyboard-keys
-        if ($this.hasClass(`left-shift`) || $this.Class(`right-shift`)) {
-          $(`.letter`).toggleClass(`uppercase`);
-          $(`.symbol span`).toggle();
 
-          shift = (shift === true) ? false : true;
-          capslock = false;
-          return false;
-        }
-        // Caps lock
-        if ($this.hasClass(`capslock`)) {
-          $(`.letter`).toggleClass(`uppercase`);
-          capslock = true;
-          return false;
-        }
-        // Delete
-        if ($this.hasClass(`delete`)) {
-          var html = $write.html();
+    //Shift keyboard-keys
+    if ($this.hasClass(`left-shift`) || $this.hasClass(`right-shift`)) {
+      $(`.letter`).toggleClass(`uppercase`);
+      $(`.symbol span`).toggle();
 
-          $write.html(html.substr(0, html.length - 1));
-          return false;
-        }
-        // Special characters
-        if ($this.hasClass(`symbol`)) character = $(`span:visible`, $this).html();
-        if ($this.hasClass(`space`)) character = ' ';
-        if ($this.hasClass(`tab`)) character = `/t`;
-        if ($this.hasClass(`return`)) character = `/n`;
-        // Uppercase letter
-        if ($this.hasClass(`Uppercase`)) character = character.toUpperCase();
-        // Remove shift once a key is clicked
-        if (shift === true) {
-          $(`.symbol span`).toggle();
-          if (capslock === false) $(`.letter`).toggleClass(`uppercase`);
+      shift = (shift === true) ? false : true;
+      capslock = false;
+      return false;
+    }
 
-          shift = false;
-        }
+    // Caps lock
+    if ($this.hasClass(`capslock`)) {
+      $(`.letter`).toggleClass(`uppercase`);
+      capslock = true;
+      return false;
+    }
 
-        // Add the character
-        $write.html($write.html() + character);
-    });
+    // Delete
+    if ($this.hasClass(`delete`)) {
+      var html = $write.html();
+
+      $write.html(html.substr(0, html.length - 1));
+      return false;
+    }
+
+    // Special characters
+    if ($this.hasClass(`symbol`)) character = $(`span:visible`, $this).html();
+    if ($this.hasClass(`space`)) character = ' ';
+    if ($this.hasClass(`tab`)) character = `/t`;
+    if ($this.hasClass(`return`)) character = `/n`;
+
+    // Uppercase letter
+    if ($this.hasClass(`Uppercase`)) character = character.toUpperCase();
+
+    // Remove shift once a key is clicked
+    if (shift === true) {
+      $(`.symbol span`).toggle();
+      if (capslock === false) $(`.letter`).toggleClass(`uppercase`);
+
+      shift = false;
+    }
+
+    // Add the character
+    $write.html($write.html() + character);
+  });
 });
