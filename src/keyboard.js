7879deac084a81158249939c4bb70faa8752b468
Brandi Soucy <bbsoucy@gmail.com>
2015-11-14 15:09:36 -0600

    keyboard js

--- src/keyboard.js
+++ src/keyboard.js
@@ -6,35 +6,44 @@ $(function() {
 $(`.keyboard-keys li`).click(function() {
     var $this = $(this),
         character = $this.html(); // If it's a lowercase letter, nothing happens to this variable
-//Shift keyboard-keys
-if ($this.hasClass(`left-shift`) || $this.Class(`right-shift`)) {
-    $(`.letter`).toggleClass(`uppercase`);
-    $(`.symbol span`).toggle();
+        //Shift keyboard-keys
+        if ($this.hasClass(`left-shift`) || $this.Class(`right-shift`)) {
+          $(`.letter`).toggleClass(`uppercase`);
+          $(`.symbol span`).toggle();
 
-    shift = (shift === true) ? false : true;
-    capslock = false;
-    return false;
-  }
-// Caps lock
-if ($this.hasClass(`capslock`)) {
-    $(`.letter`).toggleClass(`uppercase`);
-    capslock = true;
-    return false;
-}
-// Delete
-if ($this.hasClass(`delete`)) {
-    var html = $write.html();
+          shift = (shift === true) ? false : true;
+          capslock = false;
+          return false;
+        }
+        // Caps lock
+        if ($this.hasClass(`capslock`)) {
+          $(`.letter`).toggleClass(`uppercase`);
+          capslock = true;
+          return false;
+        }
+        // Delete
+        if ($this.hasClass(`delete`)) {
+          var html = $write.html();
 
-    $write.html(html.substr(0, html.length - 1));
-    return false;
-}
-// Special characters
-if ($this.hasClass(`symbol`)) character = $(`span:visible`, $this).html();
-if ($this.hasClass(`space`)) character = ' ';
-if ($this.hasClass(`tab`)) character = `/t`;
-if ($this.hasClass(`return`)) character = `/n`;
-// Uppercase letter
-if ($this.hasClass(`Uppercase`)) character = character.toUpperCase();
+          $write.html(html.substr(0, html.length - 1));
+          return false;
+        }
+        // Special characters
+        if ($this.hasClass(`symbol`)) character = $(`span:visible`, $this).html();
+        if ($this.hasClass(`space`)) character = ' ';
+        if ($this.hasClass(`tab`)) character = `/t`;
+        if ($this.hasClass(`return`)) character = `/n`;
+        // Uppercase letter
+        if ($this.hasClass(`Uppercase`)) character = character.toUpperCase();
+        // Remove shift once a key is clicked
+        if (shift === true) {
+          $(`.symbol span`).toggle();
+          if (capslock === false) $(`.letter`).toggleClass(`uppercase`);
 
-});
+          shift = false;
+        }
+
+        // Add the character
+        $write.html($write.html() + character);
+    });
 });
