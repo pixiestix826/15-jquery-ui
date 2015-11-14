9a8f1065542765cd7f565273e1a93661fd11dc0e
Brandi Soucy <bbsoucy@gmail.com>
2015-11-14 14:15:38 -0600

    keyboard styling changes

--- src/keyboard.js
+++ src/keyboard.js
@@ -1,11 +1,1 @@
-// $(function() {
-//   var $write = $(`.text-box`),
-//   var shift = false,
-//   var capslock = false,
-//
-// $(`.keys`).click(function() {
-//   var $this = $(this),
-//       character = $this.html();
-//
-// });
-// });
+
