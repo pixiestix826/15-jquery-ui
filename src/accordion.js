f8e412bd485e5b829e25fe2f5915623b8b9fcb1e
Brandi Soucy <bbsoucy@gmail.com>
2015-12-01 14:22:11 -0600

    complete accordion js

--- src/accordion.js
+++ src/accordion.js
@@ -2,7 +2,7 @@ export default function() {
   var bar = $(`.accordion__bar`);
 
   bar.on('click', function(ev) {
-    $(`.accordion__bar-ul`).slideUp();
+    $(this).parent().siblings().find(`.accordion__bar-ul`).slideUp();
     $(this).siblings().slideToggle();
     ev.preventDefault();
   });
